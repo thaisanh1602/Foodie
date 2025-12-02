fee462fa9348380ce484c01a0052f1ed1085c561
Nguyen Van Chau <chaunv.24ai@vku.udn.vn>
2025-12-02 17:59:15 +0700

    Trangchu

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\StatisticController;
+use App\Http\Controllers\SuggestionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', [HomeController::class, 'index'])->name('Home.index'); 
+Route::get('/', [HomeController::class, 'index'])->name('home'); 
+Route::get('/suggestion', [SuggestionController::class, 'index'])->name('suggestion'); 
+Route::get('/statistic', [StatisticController::class, 'index'])->name('statistic'); 
 
 
