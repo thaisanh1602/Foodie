183ab2a63b52e8dde33516e7ded06939872cb8fd
Nguyen Van Chau <chaunv.24ai@vku.udn.vn>
2025-12-03 08:34:41 +0700

    Rut gon header, footer, link community

--- resources/views/suggestion.blade.php
+++ resources/views/suggestion.blade.php
@@ -30,84 +30,10 @@
 
 <body>
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <!-- Container wrapper -->
-        <div class="container-fluid">
-            <!-- Toggle button -->
-            <button
-                data-mdb-collapse-init
-                class="navbar-toggler"
-                type="button"
-                data-mdb-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Navbar brand -->
-                <a class="navbar-brand mt-2 mt-lg-0 {{request()->routeIs('home') ? 'active' : '' }}" href="{{ route('home') }}">
-                    <img
-                        src="{{ asset('images/Foodie_Logo.png') }}"
-                        height="35"
-                        width="45"
-                        alt="Foodie"
-                        />
-                </a>
-                <!-- Left links -->
-               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" 
-           href="{{ route('home') }}">
-            Trang chủ
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('suggestion') ? 'active' : '' }}" 
-           href="{{ route('suggestion') }}">
-            Gợi ý món ăn
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" 
-           href="#">
-            Cộng đồng
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('statistic') ? 'active' : '' }}" 
-           href="{{ route('statistic') }}">
-            Thống kê
-        </a>
-    </li>
-</ul>
-                <!-- Left links -->
-            </div>
-            <!-- Collapsible wrapper -->
-
-            <!-- Right elements -->
-            <div class="d-flex align-items-center">
-            <ul class="navbar-nav flex-row">
-                <li class="nav-item me-3">
-                    <a class="nav-link" href="#">Về chúng tôi</a>
-                </li>
-                <li class="nav-item me-2">
-                    <a class="btn btn-primary btn-sm px-3 py-2 align-self-center" href="#">Đăng ký</a>
-                </li>
-            </ul>
-        </div>
-            <!-- Right elements -->
-        </div>
-        <!-- Container wrapper -->
-    </nav>
-    <!-- Navbar -->
-
-    <header>
-    
-    </header>
+   <!--Header-->
+    <div>
+        @include('layout.header')
+    </div>
 
     <section class="hero">
   <div class="container">
